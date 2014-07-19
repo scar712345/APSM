eee2a36fd17cc81185f39ee8a2548fca6160bee5
scar712345 <align-app@ALIGN-APPdeiMac.local>
2014-07-19 11:21:13 +0800

    新增SegmentAndSlider View
    新增SegmentAndSlider.h
    SegmentAndSlider.m
    SegmentAndSlider.xib
    
    刪除不需要檔案
    
    修改storyboard

---
+++ YumeKit/TemplateView/SegmentAndSlider.h
@@ -0,0 +1,19 @@
+//
+//  SegmentAndSlider.h
+//  APSM
+//
+//  Created by ALIGN-APP on 2014/7/19.
+//  Copyright (c) 2014年 yume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+IB_DESIGNABLE
+@interface SegmentAndSlider : UIView
+
+
+@property (nonatomic) IBInspectable NSString *sliderKeyPath;
+@property (nonatomic) IBInspectable NSString *viewSourceKeyPath;
+//@property (nonatomic) IBInspectable NSInteger 
+
+
+@end
---
+++ YumeKit/TemplateView/SegmentAndSlider.m
@@ -0,0 +1,103 @@
+//
+//  SegmentAndSlider.m
+//  APSM
+//
+//  Created by ALIGN-APP on 2014/7/19.
+//  Copyright (c) 2014年 yume. All rights reserved.
+//
+
+#import "SegmentAndSlider.h"
+
+@interface SegmentAndSlider()
+@property (strong, nonatomic) IBOutlet UIView *view;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (strong, nonatomic) UISegmentedControl *segmentedControl;
+
+@end
+
+
+@implementation SegmentAndSlider
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void) setup{
+    NSString *nibName = NSStringFromClass([self class]);
+    
+    NSBundle *p = [NSBundle bundleWithIdentifier:@"com.Align.YumeKit"];
+    
+    UINib *nib = [UINib nibWithNibName:nibName bundle:p];
+    
+    [nib instantiateWithOwner:self options:nil];
+    //Add the view loaded from the nib into self.
+    [self addSubview:self.view];
+    
+//    self.segmentedControl = [[UISegmentedControl alloc] initWithFrame:CGRectMake(8, 0, 284, 30)];
+//    [self.view addSubview:self.segmentedControl];
+    
+//    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(radioData:) name:@"radioData" object:[yumeBTLERemoteController sharedInstance ]];
+}
+
+-(void)prepareForInterfaceBuilder{
+    [self viewLiveRendering];
+}
+
+- (void)drawRect:(CGRect)rect{
+    
+#ifndef TARGET_INTERFACE_BUILDER
+    [self viewLiveRendering];
+#endif
+}
+
+-(void)viewLiveRendering{
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    [self.slider setMaximumTrackImage:[ UIImage imageNamed:@"slider_track(240x10).png" ] forState:UIControlStateNormal];
+    [self.slider setThumbImage:[UIImage imageNamed:@"slider.png"] forState:UIControlStateNormal];
+    self.slider.continuous = YES;
+    
+    NSArray *itemArray =[NSArray arrayWithObjects:@"?", @"??", @"123456",nil];
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:itemArray];
+    self.segmentedControl.frame = CGRectMake(8, 10, 284, 25);
+    
+    self.segmentedControl.tintColor = [UIColor whiteColor];
+    self.segmentedControl.selectedSegmentIndex = 0;
+    [self.view addSubview:self.segmentedControl];
+
+    
+    
+//    if (_sliderKeyPath) {
+//        _sliderSource = [YumeBTSharedInstance valueForKeyPath:_sliderKeyPath];
+//        self.slider.minimumValue = [[_sliderSource valueForKey:@"valueMin"] floatValue];
+//        self.slider.value = [[_sliderSource valueForKey:@"valueUI"] floatValue];
+//        self.slider.maximumValue = [[_sliderSource valueForKey:@"valueMax"] floatValue];
+//        self.labelMin.text = [[_sliderSource valueForKey:@"valueMin"] stringValue];
+//        self.labelDef.text = [[_sliderSource valueForKey:@"valueUI"] stringValue];
+//        self.labelMax.text = [[_sliderSource valueForKey:@"valueMax"] stringValue];
+//    }
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
